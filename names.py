31ed36f3ad8a2ee0bae18a4845959dee4910ed9d
Dustin Brown <Dustin Brown>
2016-05-05 18:30:47 -0400

    completed names.py

---
+++ names.py
@@ -0,0 +1,10 @@
+def names_1():
+    students = [
+        {'first_name':  'Michael', 'last_name' : 'Jordan'},
+        {'first_name' : 'John', 'last_name' : 'Rosales'},
+        {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+        {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+    for entry in students:
+        print entry['first_name'],entry['last_name']
+names_1()
